fb120eb51bcf8658ae8a421a80fbe9236a699a4c
Seungri Kang <seungrikang@Seungriui-MacBookPro.local>
2023-12-13 07:54:45 +0900

    메뉴 클릭시 자동 라우터 이동

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,14 +1,34 @@
-export default function Header() {
-	return (
-		<header className='header'>
-			<h1>SEUNGRI KANG</h1>
+import { Link, NavLink } from "react-router-dom";
 
-			<ul>
-				<li>Department</li>
-				<li>Youtube</li>
-				<li>Gallery</li>
-				<li>Location</li>
-			</ul>
-		</header>
-	);
+export default function Header() {
+  return (
+    <header className="header">
+      <h1>
+        <Link to="/">SEUNGRI KANG</Link>
+      </h1>
+
+      <ul>
+        <li>
+          <NavLink to="/Department" activeClassName={"on"}>
+            Department
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/Youtube" activeClassName={"on"}>
+            Youtube
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/Gallery" activeClassName={"on"}>
+            Gallery
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/Location" activeClassName={"on"}>
+            Location
+          </NavLink>
+        </li>
+      </ul>
+    </header>
+  );
 }
