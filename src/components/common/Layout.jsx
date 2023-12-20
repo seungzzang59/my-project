408e64e1e60eec39debc745e2e4d581b6404492a
Seungri Kang <seungrikang@Seungriui-MacBookPro.local>
2023-12-21 08:58:08 +0900

    problem

--- src/components/common/Layout.jsx
+++ src/components/common/Layout.jsx
@@ -1,6 +1,7 @@
 export default function Layout({ children, title }) {
   return (
     <main ClassName={`layout ${title}`}>
+      <figure className="subVisual"></figure>
       <h1>{title}</h1>
       {children}
     </main>
--- src/components/sub/Youtube.jsx
+++ src/components/sub/Youtube.jsx
@@ -1,9 +1,13 @@
 import Layout from "../common/Layout";
 
 export default function Youtube() {
+  const api_key = "AIzaSyAgqqcexQLVEpJEC8ATfvvyYz-9VuE5QWw";
+
   return (
     <Layout title={"Youtube"}>
-      <p>Youtube Page Contents</p>
+      <div className="con">
+        <p>Youtube Page Contents</p>
+      </div>
     </Layout>
   );
 }
