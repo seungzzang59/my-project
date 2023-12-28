f39e9f5cc8c6d861af12202ae13997e3fe00fb62
Seungri Kang <seungrikang@Seungriui-MacBookPro.local>
2023-12-28 18:21:43 +0900

    params로 전달된 값으로 상세페이지에 유튜브 영상 출력

--- src/components/sub/YoutubeDetail.jsx
+++ src/components/sub/YoutubeDetail.jsx
@@ -2,14 +2,33 @@
 
 import { useParams } from "react-router-dom";
 import Layout from "../common/Layout";
+import { useState, useEffect } from "react";
 
 function YoutubeDetail() {
-  const params = useParams();
-  console.log(params);
+  const { id } = useParams();
+  const [YoutubeData, setYoutubeData] = useState(null);
+
+  useEffect(() => {
+    const api_key = "AIzaSyAgqqcexQLVEpJEC8ATfvvyYz-9VuE5QWw";
+    const baseURL = `https://www.googleapis.com/youtube/v3/playlistItems?key=${api_key}&part=snippet&id=${id}`;
+    fetch(baseURL)
+      .then((data) => data.json())
+      .then((json) => {
+        console.log(json.items[0].snippet);
+        setYoutubeData(json.items[0].snippet);
+      });
+  }, []);
+
   return (
     <Layout title={"Detail"}>
-      <p>유튜브 상세페이지</p>
-      <p>{params.id}</p>
+      <h3>{YoutubeData?.title}</h3>
+      <div className="videoBox">
+        <iframe
+          src={`https://www.youtube.com/embed/${YoutubeData?.resourceId.videoId}`}
+          title={YoutubeData?.title}
+        ></iframe>
+      </div>
+      <p>{YoutubeData?.description}</p>
     </Layout>
   );
 }
